019204bbd7d801e8dca42e709ec20aaf1184ed00
piyawat-wir <piyawat.wir@student.mahidol.edu>
2022-11-29 19:41:18 +0700

    clean style.go

--- style.go
+++ style.go
@@ -2,33 +2,20 @@ package main
 
 import color "github.com/fatih/color"
 
-//Constant style as Object
-type StyleInterface interface {
-	mycolor(str string) string
-}
-type StyleProperty struct {
-	myint int
-}
+type StyleFunc func(str string, a ...interface{}) string
 
-func (s *StyleProperty) success(str string, a ...interface{}) string {
-	return color.New(color.FgHiGreen).Sprintf(str, a...)
-}
-func (s *StyleProperty) error(str string, a ...interface{}) string {
-	return color.New(color.FgHiRed).Sprintf(str, a...)
-}
-func (s *StyleProperty) log(str string, a ...interface{}) string {
-	return color.New(color.FgHiBlue, color.Faint).Sprintf(str, a...)
-}
-func (s *StyleProperty) title(str string, a ...interface{}) string {
-	return color.New(color.FgHiBlue, color.Bold).Sprintf(str, a...)
-}
-func (s *StyleProperty) warn(str string, a ...interface{}) string {
-	return color.New(color.FgYellow).Sprintf(str, a...)
-}
-func (s *StyleProperty) slight(str string, a ...interface{}) string {
-	return color.New(color.FgWhite, color.Faint).Sprintf(str, a...)
-}
-
-var style StyleProperty = StyleProperty{
-	myint: 3141592,
+var style = struct {
+	success StyleFunc
+	error   StyleFunc
+	warn    StyleFunc
+	log     StyleFunc
+	title   StyleFunc
+	slight  StyleFunc
+}{
+	success: color.New(color.FgHiGreen).Sprintf,
+	error:   color.New(color.FgHiRed).Sprintf,
+	warn:    color.New(color.FgYellow).Sprintf,
+	log:     color.New(color.FgHiBlue, color.Faint).Sprintf,
+	title:   color.New(color.FgHiBlue, color.Bold).Sprintf,
+	slight:  color.New(color.FgWhite, color.Faint).Sprintf,
 }
